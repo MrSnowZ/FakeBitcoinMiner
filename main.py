49c3607a289f2924113f84318183031489a871f6
Nick <62675916+MrSnowZ@users.noreply.github.com>
2022-05-04 21:28:45 -0600

    Create main.py
    - Script

---
+++ main.py
@@ -0,0 +1,25 @@
+from colorama import Fore
+import time
+import secrets
+from random import randint
+
+continuing = True
+
+btcval = 31643.79
+
+while True:
+    if continuing == True:
+        time.sleep(.01)
+        ranInt = randint(0,2500)
+        if ranInt <= 1:
+            randomBTC = randint(1,100)/100
+            print(Fore.WHITE + "y> 0x" + secrets.token_hex(20) + Fore.GREEN + " > " + str(randomBTC) + " BTC ($" + str("{:,}".format(round(btcval*randomBTC,2))) + ")")
+            answer = input("> Would you like to continue? (y/n) ")
+            if answer.lower() == "y":
+                continuing = True
+            else:
+                continuing = False
+        else:
+            print(Fore.WHITE + "> 0x" + secrets.token_hex(20) + Fore.RED + " > 0.00 BTC ($0.00)")
+    else:
+        break
